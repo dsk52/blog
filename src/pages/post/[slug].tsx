84a087fe5fb49a1b2e8ab5de7bcba2d7fd5e179a
Daisuke Konishi <skydai1151@gmail.com>
2021-11-10 02:16:37 +0900

    詳細ページにざっくり要素を置くところまで実装

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,14 @@
 import { ParsedUrlQuery } from 'node:querystring'
+import React from "react";
 
+import { ButtonLink } from "../../components/Button/Button";
 import MyHead from "../../components/Head/Head";
+import { Article, ArticleBody, ArticleFooter, ArticleHeader } from "../../components/layouts/ArticleBody/Article";
 import Page from "../../components/layouts/Page/Page";
+import detailStyle from '../../components/ui/PostItem/PostItem.module.css'
 import { getAllPost, getBySlug } from "../../libs/microcms";
 import { PostMapper } from "../../mapper/PostMapper";
+import { datetimeToDate } from "../../utilities/Date";
 
 import type { IPost } from '../../types/domain/Post';
 import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
@@ -18,18 +23,35 @@ interface Params extends ParsedUrlQuery {
 }
 
 const Detail: NextPage<PostPops> = ({ post }) => {
+  const pubDate = datetimeToDate(post.publishedAt)
+
   return (
     <Page head={
       <MyHead
-        title=""
+        title={post.title}
         description=""
         url="/post/"
       />
     }>
-      <>
-        {!post ? '' : (<div>{post.title}</div>)}
-        詳細
-      </>
+      <Article>
+        <ArticleHeader>
+          <h1>{post.title}</h1>
+          <div className={detailStyle.meta}>
+            <div className={detailStyle.category}>{post.category.name}</div>
+            <time className={detailStyle.date}>{pubDate}</time>
+          </div>
+        </ArticleHeader>
+
+        <ArticleBody>
+          {post.body}
+        </ArticleBody>
+
+        <ArticleFooter>
+          <ButtonLink link='/post'>
+            トップに戻る
+          </ButtonLink>
+        </ArticleFooter>
+      </Article>
     </Page>
   )
 }
@@ -58,7 +80,7 @@ export const getStaticProps: GetStaticProps<PostPops, Params> = async (context) 
   }
 
   const res = await getBySlug(slug)
-  if (!res.contents) {
+  if (!res.contents || !res.contents.length) {
     return {
       notFound: true
     }
