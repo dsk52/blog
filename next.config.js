233f2b3c2975ab314d875bbb0f22fe1953876375
Daisuke Konishi <skydai1151@gmail.com>
2021-11-15 01:22:48 +0900

    Revert "postをトップにリダイレクトする設定を追加 #49"
    This reverts commit 146c7e4a744ac69d82d4049abce3548eebd56e4a.

--- next.config.js
+++ next.config.js
@@ -3,14 +3,4 @@ module.exports = {
   reactStrictMode: true,
 
   pageExtensions: ['jsx', 'ts', 'tsx'],
-
-  async redirects() {
-    return [
-      {
-        source: '/post',
-        destination: '/',
-        permanent: true,
-      },
-    ]
-  },
 }
