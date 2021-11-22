8bf97da8ab669cc6d2f049d0a79d8ba36a39c223
Daisuke Konishi <skydai1151@gmail.com>
2021-11-23 01:00:00 +0900

    参照する環境変数を変更 #75

--- src/utilities/env.ts
+++ src/utilities/env.ts
@@ -1,3 +1,3 @@
-const NODE_ENV = process.env.NODE_ENV || "";
+const APP_ENV = process.env.APP_ENV || "";
 
-export const isProduction: boolean = NODE_ENV === "production";
+export const isProduction: boolean = APP_ENV === "production";
