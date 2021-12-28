4d4d2969560b44a10bde5600ec1a2818b291bfac
Daisuke Konishi <skydai1151@gmail.com>
2021-12-29 01:12:36 +0900

    hotfix TSでのenvマッピングを除外し、Next.jsのconfigを通す形に置き換え

--- src/components/templates/ListPage.tsx
+++ src/components/templates/ListPage.tsx
@@ -1,6 +1,6 @@
+import getConfig from 'next/config';
 import React, { VFC } from "react";
 
-import { envVar } from '../../constants/environment';
 import { Adsense } from "../Adsense/Adsense";
 import { Pager } from "../ui/Pager/Pager";
 import { PostList } from "../ui/PostList/PostList";
@@ -13,11 +13,19 @@ type Props = {
   posts: IPostItem[]
 } & PageBaseProp
 
+const { publicRuntimeConfig } = getConfig()
+
+const {
+  NEXT_PUBLIC_ADS_ARTICLE_TOP_SLOT,
+  NEXT_PUBLIC_ADSENSE_CLIENT,
+  NEXT_PUBLIC_ADS_ARTICLE_BOTTOM_SLOT
+} = publicRuntimeConfig
+
 export const ListPage: VFC<Props> = ({ posts, maxPage, pageNum }) => (
   <>
     <Adsense
-      client={envVar.NEXT_PUBLIC_ADSENSE_CLIENT}
-      slot={envVar.NEXT_PUBLIC_ADS_ARTICLE_TOP_SLOT}
+      client={NEXT_PUBLIC_ADSENSE_CLIENT}
+      slot={NEXT_PUBLIC_ADS_ARTICLE_TOP_SLOT}
     />
 
     <PostList posts={posts} />
@@ -30,8 +38,8 @@ export const ListPage: VFC<Props> = ({ posts, maxPage, pageNum }) => (
       />
 
       <Adsense
-        client={envVar.NEXT_PUBLIC_ADSENSE_CLIENT}
-        slot={envVar.NEXT_PUBLIC_ADS_ARTICLE_BOTTOM_SLOT}
+        client={NEXT_PUBLIC_ADSENSE_CLIENT}
+        slot={NEXT_PUBLIC_ADS_ARTICLE_BOTTOM_SLOT}
       />
     </footer>
   </>
