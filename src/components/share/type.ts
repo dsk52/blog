0d7e9b46475be07d69c8bcfd87dd6075a8adec80
Daisuke Konishi <skydai1151@gmail.com>
2022-06-05 00:22:50 +0900

    Twitterのシェアリンクを自作

---
+++ src/components/share/type.ts
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+
+export type ShareLink = {
+  url: string;
+  children: ReactNode;
+};
--- src/components/ui/Share/Share.tsx
+++ src/components/ui/Share/Share.tsx
@@ -1,21 +1,24 @@
 // import { TwitterShareButton, TwitterIcon, FacebookShareButton, FacebookIcon } from "react-share";
 
+import { SITE } from "../../../constants/site";
+import { TwitterShare } from "../../share/TwitterShare/TwitterShare";
 import s from './Share.module.css'
 
 import type { Props } from "./type";
 
 export const Share = ({ title, path }: Props) => {
-  // const url = `${SITE.url}${path}`
+  const url = `${SITE.url}${path}`
 
   return (
     <ul className={s.ShareList}>
       <li className={s.ShareListItem}>
-        {/* <TwitterShareButton
+        <TwitterShare
           title={title}
           url={url}
         >
-          <TwitterIcon size={48} round={true} />
-        </TwitterShareButton> */}
+          {/* <TwitterIcon size={48} round={true} /> */}
+          Twitter
+        </TwitterShare>
       </li>
 
       <li className={s.ShareListItem}>
