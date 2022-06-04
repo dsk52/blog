7763313d1382e5195d91fb8acfe20d316413d78b
Daisuke Konishi <skydai1151@gmail.com>
2022-06-04 23:34:19 +0900

    依存がおかしいのでreact-share廃止

--- src/components/ui/Share/Share.tsx
+++ src/components/ui/Share/Share.tsx
@@ -1,30 +1,29 @@
-import { TwitterShareButton, TwitterIcon, FacebookShareButton, FacebookIcon } from "react-share";
+// import { TwitterShareButton, TwitterIcon, FacebookShareButton, FacebookIcon } from "react-share";
 
-import { SITE } from '../../../constants/site';
 import s from './Share.module.css'
 
 import type { Props } from "./type";
 
 export const Share = ({ title, path }: Props) => {
-  const url = `${SITE.url}${path}`
+  // const url = `${SITE.url}${path}`
 
   return (
     <ul className={s.ShareList}>
       <li className={s.ShareListItem}>
-        <TwitterShareButton
+        {/* <TwitterShareButton
           title={title}
           url={url}
         >
           <TwitterIcon size={48} round={true} />
-        </TwitterShareButton>
+        </TwitterShareButton> */}
       </li>
 
       <li className={s.ShareListItem}>
-        <FacebookShareButton
+        {/* <FacebookShareButton
           url={url}
         >
           <FacebookIcon size={48} round={true} />
-        </FacebookShareButton>
+        </FacebookShareButton> */}
       </li>
     </ul>
   )
