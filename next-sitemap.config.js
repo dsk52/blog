65869ae2fae8bedba9a9e1d1cfac6451aeb361ab
Daisuke Konishi <skydai1151@gmail.com>
2023-02-14 22:51:33 +0900

    記事詳細を事前に生成するように戻した

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://blog.daisukekonishi.com',
+  siteUrl: process.env.SITE_URL || "https://blog.daisukekonishi.com",
   generateRobotsTxt: true, // (optional)
-  sitemapSize: 7000,
-  outDir: './public'
-}
+  sitemapSize: 5000,
+  outDir: "./public",
+};
