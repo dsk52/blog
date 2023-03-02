991ef79b5c6f32516e580b1df35ed00a341cd322
Daisuke Konishi <skydai1151@gmail.com>
2023-03-03 01:03:00 +0900

    ad

--- src/components/apps/Adsense/Adsense.tsx
+++ src/components/apps/Adsense/Adsense.tsx
@@ -29,17 +29,16 @@ export const Adsense = ({
   }, [asPath]);
 
   return (
-    <div key={asPath}>
-      <ins
-        className={`${className} adsbygoogle`}
-        style={style}
-        data-ad-client={client}
-        data-ad-slot={slot}
-        data-ad-layout={layout}
-        data-ad-layout-key={layoutKey}
-        data-ad-format={format}
-        data-full-width-responsive={responsive}
-      ></ins>
-    </div>
+    <ins
+      key={asPath}
+      className={`${className} adsbygoogle`}
+      style={style}
+      data-ad-client={client}
+      data-ad-slot={slot}
+      data-ad-layout={layout}
+      data-ad-layout-key={layoutKey}
+      data-ad-format={format}
+      data-full-width-responsive={responsive}
+    ></ins>
   );
 };
