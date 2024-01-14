78759c5af5eeff8f1888d3ccafc76522a6ca277e
Daisuke KONISHI <skydai1151@gmail.com>
2024-01-14 15:08:16 +0900

    Aboutのスタイル調整

--- src/components/page/About/About.tsx
+++ src/components/page/About/About.tsx
@@ -1,8 +1,5 @@
-import {
-  Article,
-  ArticleHeader,
-  ArticleBody,
-} from "@/components/layouts/Article/Article";
+import { clsx } from "clsx";
+
 import { Base } from "@/components/layouts/Base";
 import { Container } from "@/components/ui/Container/Container";
 import { Heading } from "@/components/ui/Heading/Heading";
@@ -15,67 +12,72 @@ import type { NextPage } from "next";
 export const AboutPage: NextPage = () => (
   <Base>
     <Seo />
-    <Article>
+    <article>
       <Container>
-        <ArticleHeader>
-          <Heading>About</Heading>
-        </ArticleHeader>
+        <div className={clsx("tw-space-y-4")}>
+          <header>
+            <Heading className={clsx("tw-text-4xl tw-leading-relaxed")}>
+              About
+            </Heading>
+          </header>
 
-        <ArticleBody>
-          <section>
-            <Heading as="h2">サイトについて</Heading>
-            <p>
-              仕事やプライベートで知った技術や、イベントで喋った話・行った話をブログとして残しています。
-            </p>
-            <p>
-              ペンギンが好きな事と何か残す時にノートに書き出す事が多かったのでこの名前。
-            </p>
-          </section>
+          <div className={clsx("tw-prose tw-prose-slate tw-max-w-full")}>
+            <section>
+              <Heading as="h2">サイトについて</Heading>
+              <p>
+                仕事やプライベートで知った技術や、イベントで喋った話・行った話をブログとして残しています。
+              </p>
+              <p>
+                ペンギンが好きな事と何か残す時にノートに書き出す事が多かったのでこの名前。
+              </p>
+            </section>
 
-          <section>
-            <Heading as="h2">私について</Heading>
-            <p>鹿児島生まれ京都育ち、今は大阪に住んでます。</p>
-            <p>興味駆動でWeb周りのことをやってます。</p>
-            <p>
-              Webフロントエンドとサーバーサイド、マーケティングとか好きです。
-            </p>
-            <p>好きなものは酒とカレーとラーメン。</p>
+            <section>
+              <Heading as="h2">私について</Heading>
+              <p>鹿児島生まれ京都育ち、今は大阪に住んでます。</p>
+              <p>
+                興味駆動でWeb周りのことをやってます。
+                <br />
+                Webフロントエンドとサーバーサイド、マーケティングとか好きです。
+                <br />
+                好きなものは酒とカレーとラーメン。
+              </p>
 
-            <p>
-              詳しくは<a href={EXTERNAL_PAGE.PROFILE}>こちら</a>
-            </p>
-          </section>
+              <p>
+                詳しくは<a href={EXTERNAL_PAGE.PROFILE}>こちら</a>
+              </p>
+            </section>
 
-          <section>
-            <Heading as="h2">免責事項</Heading>
+            <section>
+              <Heading as="h2">免責事項</Heading>
 
-            <p>
-              当サイトに掲載されている情報の正確さについて可能な限り努力をしていますが、その正確性や適切性に問題がある場合、告知無しに情報を変更・削除する事があります。
-            </p>
-            <p>
-              当ブログの情報を用いて行う一切の行為、被った損害・損失に対しては、一切の責任を負いかねます。ご了承ください。
-            </p>
-            <p>
-              必ず各リンク先、オフィシャルサイトなどをご参照頂き、ご自身の判断、責任にてご利用頂きますようお願い申し上げます。
-            </p>
-            <p>
-              本免責事項及び、当サイトに記載されている情報は予告なしに変更、または削除されることがあります。予めご了承下さい。
-            </p>
-          </section>
+              <p>
+                当サイトに掲載されている情報の正確さについて可能な限り努力をしていますが、その正確性や適切性に問題がある場合、告知無しに情報を変更・削除する事があります。
+                <br />
+                当ブログの情報を用いて行う一切の行為、被った損害・損失に対しては、一切の責任を負いかねます。ご了承ください。
+                <br />
+                必ず各リンク先、オフィシャルサイトなどをご参照頂き、ご自身の判断、責任にてご利用頂きますようお願い申し上げます。
+              </p>
+              <p>
+                本免責事項及び、当サイトに記載されている情報は予告なしに変更、または削除されることがあります。予めご了承下さい。
+              </p>
+            </section>
 
-          <section>
-            <Heading as="h2">お問い合わせ</Heading>
-            <p>
-              お問い合わせはTwitter <a href={EXTERNAL_PAGE.TWITTER}>@skd_nw</a>
-              までよろしくお願い致します。
-              <br />
-              <small>
-                ※名前がコロコロ変わりますが、アイコンは青いペンギンです。
-              </small>
-            </p>
-          </section>
-        </ArticleBody>
+            <section>
+              <Heading as="h2">お問い合わせ</Heading>
+              <p>
+                お問い合わせはTwitter{" "}
+                <a href={EXTERNAL_PAGE.TWITTER}>@skd_nw</a>
+                までよろしくお願い致します。
+                <br />
+                <small>
+                  ※名前がコロコロ変わりますが、アイコンは青いペンギンです。
+                </small>
+              </p>
+            </section>
+          </div>
+        </div>
       </Container>
-    </Article>
+    </article>
   </Base>
 );
