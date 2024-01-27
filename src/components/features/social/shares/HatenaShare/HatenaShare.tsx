dd3b091caae38edf360e759091e195d9c729c5c4
Daisuke KONISHI <skydai1151@gmail.com>
2024-01-27 11:50:03 +0900

    はてなブックマークのリンクを追加

---
+++ src/components/features/social/shares/HatenaShare/HatenaShare.tsx
@@ -0,0 +1,27 @@
+import { clsx } from "clsx";
+import Script from "next/script";
+
+import { HatenaIcon } from "@/components/icons/Hatena/Hatena";
+
+export const HatenaShare = () => {
+  return (
+    <div>
+      <a
+        href="https://b.hatena.ne.jp/entry/"
+        className={clsx(
+          "tw-transition-opacity hover:tw-opacity-70",
+          "hatena-bookmark-button",
+        )}
+        data-hatena-bookmark-layout="touch"
+      >
+        <HatenaIcon size="36" />
+      </a>
+
+      <Script
+        type="text/javascript"
+        src="https://b.st-hatena.com/js/bookmark_button.js"
+        async
+      ></Script>
+    </div>
+  );
+};
