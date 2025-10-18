85c565cc77245f604c36ae5d196a12caab2c1547
Daisuke KONISHI <skydai1151@gmail.com>
2025-10-18 13:47:38 +0900

    feat: パンくず追加

--- src/components/page/TagIndex/TagIndex.tsx
+++ src/components/page/TagIndex/TagIndex.tsx
@@ -1,6 +1,8 @@
 import { CommonLayout } from "@/components/layouts/CommonLayout";
+import { Breadcrumb } from "@/components/ui/Breadcrumb";
 import { Container } from "@/components/ui/Container/Container";
 import { TagList } from "@/components/ui/TagList/TagList";
+import { ROUTE } from "@/constants/route";
 
 import type { TagsPageProp } from "./type";
 
@@ -8,6 +10,10 @@ export const TagIndexPage = ({ tags }: TagsPageProp) => (
   <CommonLayout>
     <Container>
       <TagList tags={tags} doLink />
+      <Breadcrumb
+        items={[{ label: "ホーム", href: ROUTE.top }, { label: "タグ一覧" }]}
+        className="tw:mt-6"
+      />
     </Container>
   </CommonLayout>
 );
--- src/components/page/TagList/TagList.tsx
+++ src/components/page/TagList/TagList.tsx
@@ -4,6 +4,7 @@ import { Adsense } from "@/components/apps/Adsense/Adsense";
 import { PostList } from "@/components/features/post/PostList/PostList";
 import { CommonLayout } from "@/components/layouts/CommonLayout";
 import type { TagListPageProp } from "@/components/page/TagList/type";
+import { Breadcrumb } from "@/components/ui/Breadcrumb";
 import { Container } from "@/components/ui/Container/Container";
 import { Pager } from "@/components/ui/Pager/Pager";
 import { AdsenseClient, AdsenseUnits } from "@/constants/google";
@@ -20,12 +21,21 @@ export const TagListPage = ({ tag, posts, maxPage, pageNum }: TagListPageProp) =
         <section>
           <Container>
             <PostList posts={posts} />
+
+            <Pager basePath={ROUTE.postTagListBy(tag.slug)} maxPage={maxPage} pageNum={pageNum} />
           </Container>
         </section>
 
         <footer>
           <Container>
-            <Pager basePath={ROUTE.postTagListBy(tag.slug)} maxPage={maxPage} pageNum={pageNum} />
+            <Breadcrumb
+              items={[
+                { label: "ホーム", href: ROUTE.top },
+                { label: "タグ一覧", href: ROUTE.tagList },
+                { label: `${tag.name}タグの記事一覧` },
+              ]}
+              className="tw:mb-6"
+            />
 
             <Adsense
               client={AdsenseClient}
