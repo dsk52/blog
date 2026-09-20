585518b466b5656fbca71e0827a02ab2d9796746
Daisuke KONISHI <skydai1151@gmail.com>
2026-09-21 02:27:21 +0900

    refactor(test): Vite標準のtsconfigパス解決を利用

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,12 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [react()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     globals: true,
     environment: 'happy-dom',
